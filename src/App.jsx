66dfb4a7a93b17242148d374e5f4ad05618e2339
Dhivyaprasad M <dhivyaprasad@Dhivyaprasads-MacBook-Air.local>
2025-07-17 12:02:21 +0530

    Updated css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold underline">
+      <h1 className="text-4xl font-bold underline bg-red-400">
         Hello world!
       </h1>
       <h1>welcome to my updated site</h1>
